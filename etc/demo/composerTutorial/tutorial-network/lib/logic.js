a9f75421faf5e99efb4df1f9dbae4474cf834dc2
RaikireHiuduo <chun_heng@hotmail.com>
2017-12-20 15:48:09 +0800

    Part 3 of composerDemo; building the transaction logic
    The functions or transaction that can be called. Based on what is defined in cto.

--- etc/demo/composerTutorial/tutorial-network/lib/logic.js
+++ etc/demo/composerTutorial/tutorial-network/lib/logic.js
@@ -1,23 +1,15 @@
 'use strict';
-/**
- * Write your transction processor functions here
- */
 
 /**
- * Sample transaction
- * @param {org.acme.biznet.ChangeAssetValue} changeAssetValue
+ * Track the trade of a commodity from one trader to another
+ * @param {org.acme.biznet.Trade} trade - the trade to be processed
  * @transaction
  */
-function onChangeAssetValue(changeAssetValue) {
-    var assetRegistry;
-    var id = changeAssetValue.relatedAsset.assetId;
-    return getAssetRegistry('org.acme.biznet.SampleAsset')
-        .then(function(ar) {
-            assetRegistry = ar;
-            return assetRegistry.get(id);
-        })
-        .then(function(asset) {
-            asset.value = changeAssetValue.newValue;
-            return assetRegistry.update(asset);
+
+function tradeCommodity(trade) {
+    trade.commodity.owner = trade.newOwner;
+    return getAssetRegistry('org.acme.biznet.Commodity')
+        .then(function (assetRegistry) {
+            return assetRegistry.update(trade.commodity);
         });
 }
