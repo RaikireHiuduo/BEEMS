e83dc6e23cfcfe3229ecfad58a400c77b659e991
RaikireHiuduo <chun_heng@hotmail.com>
2017-12-28 17:30:07 +0800

    Base .cto. Draft lib/logic.js. Done .acl.
    cto file is based on what was mentioned in the slide for the blockchain side. Having a hard time understanding for addition and reading for the logic part so it only contains placeholders. acl is just a cp with network name fix

--- main/beems/lib/logic.js
+++ main/beems/lib/logic.js
@@ -1,16 +1,33 @@
 'use strict';
+
 /**
  * Write your transction processor functions here
  */
+
+function doAddDigitalSignature (addDigitalSignature) {
+    //TODO.
+
+    //I dunno how Composer works for C---. Just dump it? >_>
+}
+
+function doGetDigitalSignature (getDigitalSignature) {
+    //TODO.
+
+    //Get the Digital Signature list.
+    //Check by TID, return full list.
+    return getAssetRegistry('org.bit.beems.DigitalSignature')
+        .then()
+}
 
 /**
  * Sample transaction
  * @param {org.bit.beems.ChangeAssetValue} changeAssetValue
  * @transaction
- */
+
 function onChangeAssetValue(changeAssetValue) {
     var assetRegistry;
     var id = changeAssetValue.relatedAsset.assetId;
+
     return getAssetRegistry('org.bit.beems.SampleAsset')
         .then(function(ar) {
             assetRegistry = ar;
@@ -21,3 +38,5 @@ function onChangeAssetValue(changeAssetValue) {
             return assetRegistry.update(asset);
         });
 }
+
+*/
